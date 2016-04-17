e8c060f84f87886e4821bc3e3964cce26c2c2f7a
Mustafa Temel <mustafatemel90@gmail.com>
2016-04-17 03:04:42 +0000

    Added scroll navigation.

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,23 @@
     
     try {
         if (typeof $j === 'function') {
-            $j('#bb-navbar a').click(function() {
+            $j('#bb-navbar a').click(function(event) {
+                // Prevent default behaviour
+                event.preventDefault();
+                
+                // Highlight active 'page'
                 $j('#bb-navbar li.active').removeClass('active');
                 $j(this).parent().addClass('active');
+                
+                // Collapse navigation bar (mobile)
+                $j('[data-target="#bb-navbar"]:not(".collapsed")').click();
+                
+                // Set current 'page'
+                g.currentPage = $j(this).prop('href').substring($j(this).prop('href').indexOf('#'));
+                
+                $j('html, body').animate({
+                    scrollTop: (g.currentPage === '#home') ? 0 : $j(g.currentPage).offset().top
+                }, 500);
             });
         }
     } catch (exception) {
