6eb87b101e0a6cfa50ecdc7ea29ebf74a9f7f7cf
temelm <mustafatemel90@gmail.com>
2017-08-22 23:01:01 +0100

    Converted indentation to 2 spaces.

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
-    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
-        $to = "mustafatemel90@gmail.com"; // belsizebathrooms@btconnect.com 
-        $subject = $_POST["name"]." - ".$_POST["email"];
-        $message = $_POST["message"]."\n\n--------\n\n".$_POST["name"]."\n".$_POST["email"];
-        $message = wordwrap($message, 120);
-        
-        //mail($to, $subject, $message);
-    }
+  if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
+    $to = "mustafatemel90@gmail.com"; // belsizebathrooms@btconnect.com 
+    $subject = $_POST["name"]." - ".$_POST["email"];
+    $message = $_POST["message"]."\n\n--------\n\n".$_POST["name"]."\n".$_POST["email"];
+    $message = wordwrap($message, 120);
     
-    //header("Location: ./index.html");
+    //mail($to, $subject, $message);
+  }
+  
+  //header("Location: ./index.html");
 ?>
--- src/js/index.js
+++ src/js/index.js
@@ -1,75 +1,75 @@
 (function init(g, d, c, $j) {
-    'use strict';
-    
-    try {
-        if (typeof $j === 'function') {
-            g.scrollSpeed = 300;
-            
-            // Open store links in a new window/tab
-            $j('#store .list-group-item a').prop('target', '_blank');
-            
-            // Handle navigation link clicks
-            $j('#bb-navbar a').click(function(event) {
-                event.preventDefault();
-                
-                // Collapse navigation bar (mobile)
-                $j('[data-target="#bb-navbar"]:not(".collapsed")').click();
-                
-                // Set current hash
-                var hash = $j(this).prop('hash');
-                
-                // Scroll to current hash
-                $j('html, body').animate({
-                    scrollTop: (hash === '#home') ? 0 : $j(hash).offset().top
-                }, g.scrollSpeed);
-            });
-            
-            // Handle 'Back to top' link clicks
-            $j('.back-to-top-link').click(function(event) {
-                event.preventDefault();
-                
-                $j('html, body').animate({
-                    scrollTop: 0
-                }, g.scrollSpeed);
-            });
-            
-            // Handle 'home' section link clicks
-            $j('#home .lead a').click(function(event) {
-                event.preventDefault();
-                
-                $j('html, body').animate({
-                    scrollTop: $j($j(this).prop('hash')).offset().top
-                }, g.scrollSpeed);
-            });
-        }
-    } catch (exception) {
-        if (!!c && typeof c === 'object' && typeof c.error === 'function') {
-            c.error('[init]:', exception.message);
-        }
+  'use strict';
+  
+  try {
+    if (typeof $j === 'function') {
+      g.scrollSpeed = 300;
+      
+      // Open store links in a new window/tab
+      $j('#store .list-group-item a').prop('target', '_blank');
+      
+      // Handle navigation link clicks
+      $j('#bb-navbar a').click(function(event) {
+        event.preventDefault();
+        
+        // Collapse navigation bar (mobile)
+        $j('[data-target="#bb-navbar"]:not(".collapsed")').click();
+        
+        // Set current hash
+        var hash = $j(this).prop('hash');
+        
+        // Scroll to current hash
+        $j('html, body').animate({
+          scrollTop: (hash === '#home') ? 0 : $j(hash).offset().top
+        }, g.scrollSpeed);
+      });
+      
+      // Handle 'Back to top' link clicks
+      $j('.back-to-top-link').click(function(event) {
+        event.preventDefault();
+        
+        $j('html, body').animate({
+          scrollTop: 0
+        }, g.scrollSpeed);
+      });
+      
+      // Handle 'home' section link clicks
+      $j('#home .lead a').click(function(event) {
+        event.preventDefault();
+        
+        $j('html, body').animate({
+          scrollTop: $j($j(this).prop('hash')).offset().top
+        }, g.scrollSpeed);
+      });
     }
+  } catch (exception) {
+    if (!!c && typeof c === 'object' && typeof c.error === 'function') {
+      c.error('[init]:', exception.message);
+    }
+  }
 })(window, window.document, window.console, window.jQuery);
 
 window.initMap = function() {
-    'use strict';
-    
-    try {
-        var latLng = {
-            lat: 51.543252,
-            lng: -0.179318
-        };
-        var map = new window.google.maps.Map(window.document.getElementById('map'), {
-            center: latLng,
-            scrollwheel: false,
-            zoom: 15
-        });
-        var marker = new window.google.maps.Marker({
-            map: map,
-            position: latLng,
-            title: 'Belsize Bathrooms'
-        });
-    } catch (exception) {
-        if (!!window.console && typeof window.console === 'object' && typeof window.console.error === 'function') {
-            window.console.error('[initMap]:', exception.message);
-        }
+  'use strict';
+  
+  try {
+    var latLng = {
+      lat: 51.543252,
+      lng: -0.179318
+    };
+    var map = new window.google.maps.Map(window.document.getElementById('map'), {
+      center: latLng,
+      scrollwheel: false,
+      zoom: 15
+    });
+    var marker = new window.google.maps.Marker({
+      map: map,
+      position: latLng,
+      title: 'Belsize Bathrooms'
+    });
+  } catch (exception) {
+    if (!!window.console && typeof window.console === 'object' && typeof window.console.error === 'function') {
+      window.console.error('[initMap]:', exception.message);
     }
+  }
 };
